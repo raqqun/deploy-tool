1c2eb8e613b5679677cd1921fd62e85f682be194
alexandros <alexandros@thebeautyst.com>
2014-08-25 12:43:09 +0200

    add constant for site url

--- app_paths.php
+++ app_paths.php
@@ -1,5 +1,6 @@
 <?php
 # Define paths constants to use in the project
+define('DEPLOY_URL', 'http://'.$_SERVER['SERVER_NAME']);
 define('ABSPATH', dirname(__FILE__));
 define('STATIC_DIR', ABSPATH.'/static');
 define('APP_DIR', ABSPATH.'/app');
