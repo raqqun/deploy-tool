403fbdbfef2d42e25e3d1a01263c94ded9ba8005
alexandros <alexandros@thebeautyst.com>
2014-08-25 11:46:28 +0200

    define absolute paths constants to use in the project

---
+++ app_paths.php
@@ -0,0 +1,7 @@
+<?php
+# Define paths constants to use in the project
+define('ABSPATH', dirname(__FILE__));
+define('STATIC_DIR', ABSPATH.'/static/');
+define('APP_DIR', ABSPATH.'/app/');
+define('TEMPLATES', ABSPATH.'/templates/');
+
