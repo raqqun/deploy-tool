fffda0e1434d61f02d7a0d003175cfb609496428
alexandros <alexandros@thebeautyst.com>
2014-08-25 14:37:02 +0200

    add slash to specify template part

--- templates/monitoring.php
+++ templates/monitoring.php
@@ -2,7 +2,7 @@
 require_once '../app_paths.php';
 include APP_DIR.'/tb_deploy_tool_view.php';
 
-echo $view->render('header');
+echo $view->render('/header');
 
 
-echo $view->render('footer');
+echo $view->render('/footer');
